0ee34de7351b0231ddea3cfa190354ad6dbd1138
cntpdlab <cntpdlab@gmail.com>
2025-11-07 16:51:30 +0200

    add sitemap and SEO config

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://cntpdlab.xyz', // твій домен без / в кінці
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+};
